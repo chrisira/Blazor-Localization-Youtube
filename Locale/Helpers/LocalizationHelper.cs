546efc1a1a0bc19de23c4fa7a983967bba40e507
Bishop <christianiradukunda6@gmail.com>
2023-11-27 13:02:25 +0200

    uplaoded all files

---
+++ Locale/Helpers/LocalizationHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Locale.Helpers
+{
+    public class LocalizationHelper
+    {
+        private readonly IConfiguration _config;
+
+        public LocalizationHelper(IConfiguration config)
+        {
+            _config = config;
+        }
+        public RequestLocalizationOptions GetLocalizationOptions()
+        {
+            var cultures = _config.GetSection("Cultures").GetChildren().
+                ToDictionary(x => x.Key, x => x.Value);
+            var supportedCultures = cultures.Keys.ToArray();
+
+            var localizationOptions = new RequestLocalizationOptions().
+                AddSupportedCultures(supportedCultures).AddSupportedUICultures(supportedCultures);
+
+            return localizationOptions;
+                
+
+        }
+    }
+}
